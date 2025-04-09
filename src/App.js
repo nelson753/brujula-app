6cdce1cf862c6fb93feb63d9dd94081ffff331da
Nelson <nelson753@gmail.com>
2025-04-08 19:29:42 -0700

    Ahora si, todos los archivos listos

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>¡Bienvenido a Brujula App!</h1>
+        <p>
+          Tu camino hacia el autodescubrimiento empieza aqui. Reflexiona, crece y conecta con tigo mismo/a.
+
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
