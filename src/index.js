dc2bacd063e0ea3a80f95682c49af1c3cb7c7753
Nelson <nelson753@gmail.com>
2025-04-09 07:50:54 -0700

    Activado service worker para convertir en PWA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './App.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 👈 esto es nuevo
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// 👇 activa el service worker para convertir la app en PWA
+serviceWorkerRegistration.register();
